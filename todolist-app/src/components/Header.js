d2a1d6dd2e64595bcf94823efa52ea0d1c405d28
Hilario Nengare <hnengare@gmail.com>
2024-04-30 19:49:56 +0300

    Implemented Header component

---
+++ todolist-app/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <div>
+                <h1 className='h1 header-title'>
+                    My Day
+                </h1>
+                
+            </div>
+        </header>
+    );
+}
+
+export default Header;
