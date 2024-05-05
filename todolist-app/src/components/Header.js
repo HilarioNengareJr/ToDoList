a03a0a3358a5a02a11861763ea2014a91dd11e35
Hilario Nengare <hnengare@gmail.com>
2024-05-05 03:56:31 +0300

    implemented header event handlers

--- todolist-app/src/components/Header.js
+++ todolist-app/src/components/Header.js
@@ -5,9 +5,10 @@ import './Header.css';
 
 const Header = () => {
 
-    // date state
-
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [isDropdownMenuActive, setDropdownMenuActive] = useState(false);
+    const [isTogglerActive, setTogglerActive] = useState(false);
+    const [isModalActive, setModalActive] = useState(false);
 
     const formattedDate = currentDate.toLocaleDateString('en-US', {
         weekday: "long",
@@ -28,6 +29,19 @@ const Header = () => {
         { hue: 206, isActive: false }
     ];
 
+    const toggleMenu = () => {
+        setTogglerActive(!isTogglerActive);
+        setDropdownMenuActive(!isDropdownMenuActive);
+    };
+
+    const toggleModal = () => setModalActive(!isModalActive);
+
+    const toggleCallBacks = () => {
+        toggleMenu();
+        toggleModal();
+    }
+
+
     return (
         <header className='header'>
             <div>
@@ -39,13 +53,13 @@ const Header = () => {
                 </p>
 
                 <div className='dropdown-wrapper'>
-                    <button className='menu-btn' aria-label='Toggle menu'>
+                    <button className={isTogglerActive ? 'menu-btn active' : 'menu-btn'} aria-label='Toggle menu' onClick={toggleMenu}>
                         <ion-icon name="ellipsis-horizontal" aria-hidden="true"></ion-icon>
                     </button>
 
                     {/* Dropdown menu */}
 
-                    <div className='dropdown-container'>
+                    <div className={isDropdownMenuActive ? 'dropdown-container active' : 'dropdown-container'} >
                         <ul className='dropdown-list'>
                             <li className='dropdown-item'>
                                 <p className='item-title'>
@@ -55,9 +69,15 @@ const Header = () => {
                                 <div className='theme-btn-group'>
                                     {
                                         buttons.map(button =>
-                                            <ThemeBtn hue={button.hue} isActive={button.isActive} />)
+                                            <ThemeBtn key={button.hue} hue={button.hue} isActive={button.isActive} />)
                                     }
                                 </div>
+                            </li>
+                            <li>
+                                <button className={isTogglerActive || isModalActive ? 'dropdown-link active' : 'dropdown-link'} onClick={toggleCallBacks}>
+                                    <ion-icon name="information-circle-outline"></ion-icon>
+                                    <span className='span'>App info</span>
+                                </button>
                             </li>
                         </ul>
                     </div>
--- todolist-app/src/index.js
+++ todolist-app/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import App from './App';
 import './index.css';
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = createRoot(document.getElementById('root'));
 
 rootElement.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals(console.log);
 
